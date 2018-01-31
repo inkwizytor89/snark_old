e06883a5c228b773479260708b529420b6e70fa3
Kamil Kostyszyn <inkwizytor89@gmail.com>
2018-01-31 20:39:39 +0100

    create status to modules

--- src/main/java/org/enoch/snark/module/AbstractModule.java
+++ src/main/java/org/enoch/snark/module/AbstractModule.java
@@ -8,6 +8,7 @@ public abstract class AbstractModule implements Comparable<AbstractModule> {
     protected static final Logger log = Logger.getLogger( AbstractModule.class.getName() );
 
     protected Date readyOn = new Date();
+    private ModuleStatus status = ModuleStatus.WAITING;
     protected Double priority = 0.0;
     private String name = this.getClass().getName();
 
@@ -17,7 +18,7 @@ public abstract class AbstractModule implements Comparable<AbstractModule> {
     }
 
     public boolean isReady() {
-        return new Date().after(readyOn);
+        return new Date().after(readyOn) && status.equals(ModuleStatus.WAITING);
     }
 
     public abstract void run();
@@ -28,5 +29,13 @@ public abstract class AbstractModule implements Comparable<AbstractModule> {
 
     public String getName() {
         return name;
+    }
+
+    public ModuleStatus getStatus() {
+        return status;
+    }
+
+    protected void setStatus(ModuleStatus status) {
+        this.status = status;
     }
 }
