242275fdbabe2ce66f0eb78a88c71c1f00e8e493
Kamil Kostyszyn <kamil.kostyszyn@capgemini.com>
2018-02-07 18:29:54 +0100

    fix time test

--- src/test/java/org/enoch/snark/common/DateUtilTest.java
+++ src/test/java/org/enoch/snark/common/DateUtilTest.java
@@ -2,7 +2,6 @@ package org.enoch.snark.common;
 
 import org.junit.Test;
 
-import java.text.ParseException;
 import java.time.LocalTime;
 
 import static org.junit.Assert.assertEquals;
@@ -10,12 +9,12 @@ import static org.junit.Assert.assertEquals;
 public class DateUtilTest {
 
     @Test
-    public void getDateFromTimeSreing() throws ParseException {
-        String timeInput="31:01:19 h";
+    public void getDateFromTimeSreing() {
+        String timeInput="21:01:19 h";
 
         final LocalTime time = DateUtil.getDateFromTimeSreing(timeInput);
 
-        assertEquals(31,time.getHour());
+        assertEquals(21,time.getHour());
         assertEquals(1,time.getMinute());
         assertEquals(19,time.getSecond());
     }
